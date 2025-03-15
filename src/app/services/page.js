0ff3dd804d37847161d9fa6747ef755a169b1012
Umar Aslam <umar.aslam@tmrconsult.com>
2025-03-15 22:32:33 +0500

    "commented files for build error"

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -1,18 +1,18 @@
 import ProcessTimeline from '@/pages/About/ProcessTimeline'
 import ServicesSection from '@/pages/Home/WhatWeDo'
-import Construction from '@/pages/Services/Construction'
+// import Construction from '@/pages/Services/Construction'
 
-import ServicesHeader from '@/pages/Services/ServicesHeader'
+// import ServicesHeader from '@/pages/Services/ServicesHeader'
 
 
 
 const Services = () => {
   return (
     <div>
-      <ServicesHeader/>
+      {/* <ServicesHeader/> */}
       <ServicesSection/>
       <ProcessTimeline/>
-      <Construction/>
+      {/* <Construction/> */}
     </div>
   )
 }
